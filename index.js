ba8005ad61a0eec9dbf9c1628b196119b2abaa38
eason0208 <eason0208@163.com>
2022-03-11 06:55:05 +0800

    更改文件名

--- index.js
+++ index.js
@@ -2,9 +2,9 @@
 
 import { createClient, segment } from 'oicq';
 import path from 'path';
-import botCommand from './lib/botCommand.js';
+import botCmd from './lib/botCmd.js';
 import init from './lib/init.js';
-import messageTemplate from './lib/messageTemplate.js';
+import msgT from './lib/msgT.js';
 import { gtgCmd } from './lib/gTGame.js';
 
 const botVer = 'v2.2.0';
@@ -39,63 +39,63 @@ client.on('system.online', () => {
 });
 
 client.on('notice.group.increase', (data) => {
-	client.sendGroupMsg(data.group_id, messageTemplate.welcomeNewMember(data));
+	client.sendGroupMsg(data.group_id, msgT.welcomeNewMember(data));
 });
 
 client.on('message.group', async (data) => {
 	try {
 		// 先检查发言者有无未读留言
-		let noticeMsg = botCommand.checkNotice(data);
+		let noticeMsg = botCmd.checkNotice(data);
 		if (noticeMsg != undefined)
 			client.sendGroupMsg(data.group_id, noticeMsg);
 
 		// bot 开关操作优先级最高
 		if (data.raw_message == '/on')
-			client.sendGroupMsg(data.group_id, botCommand.turnOn(data));
+			client.sendGroupMsg(data.group_id, botCmd.turnOn(data));
 
 		if (data.raw_message == '/off')
-			client.sendGroupMsg(data.group_id, botCommand.turnOff(data));
+			client.sendGroupMsg(data.group_id, botCmd.turnOff(data));
 
 		// 判断 bot 开关状态
 		if (
-			botCommand.onoffData[data.group_id] != undefined &&
-			botCommand.onoffData[data.group_id] == false
+			botCmd.onoffData[data.group_id] != undefined &&
+			botCmd.onoffData[data.group_id] == false
 		)
 			return;
 
 		if (data.raw_message == '/help')
-			client.sendGroupMsg(data.group_id, botCommand.help(botVer));
+			client.sendGroupMsg(data.group_id, botCmd.help(botVer));
 
 		if (data.raw_message == '/ping')
 			client.sendGroupMsg(data.group_id, 'pong!');
 
 		if (data.raw_message.slice(0, 6) == '/admin') {
 			if (data.sender.user_id == botConfig.master)
-				client.sendGroupMsg(data.group_id, botCommand.setAdmin(data));
+				client.sendGroupMsg(data.group_id, botCmd.setAdmin(data));
 			else client.sendGroupMsg(data.group_id, '没有权限!');
 		}
 
 		if (data.raw_message == '/jrrp')
-			client.sendGroupMsg(data.group_id, botCommand.getJrrp(data));
+			client.sendGroupMsg(data.group_id, botCmd.getJrrp(data));
 
 		if (data.raw_message == '/hitokoto')
-			client.sendGroupMsg(data.group_id, await botCommand.getHitokoto());
+			client.sendGroupMsg(data.group_id, await botCmd.getHitokoto());
 
 		if (data.raw_message.slice(0, 6) == '/binfo') {
 			// 正则截取 av / bv 号
 			const res = /\/binfo (\w+)/.exec(data.raw_message);
 			client.sendGroupMsg(
 				data.group_id,
-				await botCommand.getBiliInfo(res[1])
+				await botCmd.getBiliInfo(res[1])
 			);
 		}
 
 		if (data.raw_message.slice(0, 5) == '/roll') {
-			client.sendGroupMsg(data.group_id, botCommand.roll(data));
+			client.sendGroupMsg(data.group_id, botCmd.roll(data));
 		}
 
 		if (data.raw_message.slice(0, 7) == '/notice') {
-			client.sendGroupMsg(data.group_id, botCommand.setNotice(data));
+			client.sendGroupMsg(data.group_id, botCmd.setNotice(data));
 		}
 
 		if (data.raw_message == '/cat') {
@@ -108,7 +108,7 @@ client.on('message.group', async (data) => {
 
 		if (data.raw_message == '/porpic') {
 			client.sendGroupMsg(data.group_id, '获取图片中...');
-			let pic = await botCommand.getPornography(data);
+			let pic = await botCmd.getPornography(data);
 
 			// 判断状态码
 			if (pic.status / 10 != 20 || pic.url == undefined) {
